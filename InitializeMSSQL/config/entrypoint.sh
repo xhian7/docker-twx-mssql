ee0fe9de1148f9add3d85aa27fe2c63e0616649d
Christian Guzmán <viinc@viincPro.local>
2023-08-18 17:45:46 -0600

    Thingworx MSSQL dockerized

---
+++ InitializeMSSQL/config/entrypoint.sh
@@ -0,0 +1,6 @@
+PATH=$PATH:/opt/mssql-tools/bin
+{ sleep 7 ; sqlcmd -s localhost -U sa -P $MSSQL_SA_PASSWORD \
+ -l 60 -i /tmp/twx/init.sql ; \
+cd /tmp/twx/install ; ./thingworxMssqlDBSetup.sh -r $MSSQL_SA_PASSWORD; \
+./thingworxMssqlSchemaSetup.sh -r $SQL_TWX_PASSWD ; } \
+ & /opt/mssql/bin/sqlservr
---
+++ ThingworxPlatform/config/setenv.sh
@@ -0,0 +1,1 @@
+CATALINA_OPTS="$CATALINA_OPTS -Djava.library.path=${CATALINA_BASE}/webapps/Thingworx/WEB-INF/extensions -Duser.timezone=UTC"
