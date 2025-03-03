4802b11e5d3637191bf9fd490a033470f3c8ff5c
Christian Guzmán <xhian.viinc@gmail.com>
2025-03-03 09:32:32 -0600

    TWX 9.7 ready

--- ThingworxPlatform/config/setenv.sh
+++ ThingworxPlatform/config/setenv.sh
@@ -1,1 +1,14 @@
-CATALINA_OPTS="$CATALINA_OPTS -Djava.library.path=${CATALINA_BASE}/webapps/Thingworx/WEB-INF/extensions -Duser.timezone=UTC"
+CATALINA_OPTS="$CATALINA_OPTS -Djava.library.path=${CATALINA_BASE}/webapps/Thingworx/WEB-INF/extensions -Duser.timezone=UTC \
+--add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+--add-opens=java.base/java.net=ALL-UNNAMED \
+--add-opens=java.base/java.nio=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED \
+--add-opens=java.base/java.util.stream=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
+--add-opens=java.xml/com.sun.org.apache.xerces.internal.dom=ALL-UNNAMED \
+--add-opens=java.base/java.util.regex=ALL-UNNAMED \
+-Djava.net.preferIPv4Stack=true \
+-Djava.awt.headless=true -Dserver -XX:+UseNUMA \
+-XX:+UseG1GC -Dfile.encoding=UTF-8 -Dlog4j2.formatMsgNoLookups=true \
+-Xms1000M  \
+-Xmx1200M"
